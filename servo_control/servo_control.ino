8e481ec8c243568e500356b1b80d9138f4e1e296
wunianchen <36548290+wunianchen@users.noreply.github.com>
2018-03-10 01:06:17 -0500

    Motor debug passed. Need to combined Bluetooth tomorrow.

--- servo_control/servo_control.ino
+++ servo_control/servo_control.ino
@@ -1,68 +1,93 @@
-#include <AFMotor.h>                            // include the motor shield library
-#include <Servo.h>                              // include the servo library
+#include <Wire.h>
+#include <Adafruit_MotorShield.h>                   // include the motor shield library
+#include "utility/Adafruit_MS_PWMServoDriver.h"
+#include <Servo.h>                                  // include the servo library
 
-#define rightSpeed 160                          // The speed ranges from 0 (stopped) to 255 (full speed)
-#define leftSpeed 110
+// Create the motor shield object with the default I2C address
+Adafruit_MotorShield AFMS = Adafruit_MotorShield();
 
-AF_DCMotor leftMotor(1, MOTOR12_8KHZ);          // Create the AF_DCMotor Object. The first parameter: port number
-AF_DCMotor rightMotor(2, MOTOR12_8KHZ);         // Frequency: how fast the spead controlling signal is.
+//Connect the two DC motor to port M1 & M2
+Adafruit_DCMotor *L_Motor = AFMS.getMotor(1);       // robot left motor
+Adafruit_DCMotor *R_Motor = AFMS.getMotor(2);       // robot right motor
+Servo grab_servo;                                   // define servo name
 
-Servo servo1;                                   // Define servo name
-
-String readString;
+int Init_Speed = 200;                               // set the initial motor speed
+int F_Speed = 255;                                  // set the forward speed
+int B_Speed = 200;				    // set the backward speed
+int F_STOP = 0;					    // set the stop speed
+String readString;                                  // Read data from UART/Bluetooth
 
 void setup() {
   Serial.begin(9600);
-  servo1.attach(10);                            // Digital 10 pin controls the Servo#1 input
-  servo1.write(175);                            // Servo motor starting position
+  Serial.println("Collector Tivan Initialization!!!");    // initialization message
+
+  AFMS.begin();                                           // create with the default frequency 1.6 KHz.
+  grab_servo.attach(10);                                  // Digital 10 pin controls the Servo#1 input
+  grab_servo.write(175);                                  // Servo motor starting position
+
+  // Turn on left motor
+  L_Motor -> setSpeed(Init_Speed);
+  L_Motor -> run(RELEASE);
   
-  rightMotor.setSpeed(rightSpeed);
-  leftMotor.setSpeed(leftSpeed);
+  // Turn on right motor
+  R_Motor -> setSpeed(Init_Speed);
+  R_Motor -> run(RELEASE);
 }
 
+int i;
+
 void loop() {
-  // TODO: Get character from bluetooth module
-  while(Serial.available()){
-    delay(50);
-    char c=Serial.read();
-    readString+=c;
-  }
-  if(readString.length()>0){
-    Serial.println(readString);
-
-    if (readString =="FORWARD"){
-      rightMotor.run (FORWARD);
-      leftMotor.run (FORWARD);
+    while(Serial.available()){
+    	delay(50);
+    	char c=Serial.read();
+    	readString+=c;
     }
-    
-    if (readString =="BACK"){
-      rightMotor.run (BACKWARD);
-      leftMotor.run (BACKWARD);
-    }
-    
-    if (readString =="LEFT"){
-      rightMotor.run (FORWARD);
-      leftMotor.run (BACKWARD);
-    }
-    
-    if (readString =="RIGHT"){
-      rightMotor.run (BACKWARD);
-      leftMotor.run (FORWARD);
-    }
-    
-    if (readString =="STOP"){
-      rightMotor.run (RELEASE);
-      leftMotor.run (RELEASE);
-    }
-
-    if (readString == "GRAB"){
-      servo1.write(90);
-    }
-
-    if (readString == "DROP"){
-      servo1.write(175);
-    }
-
-    readString="";
-  }
+      
+    if(readString.length()>0){
+	    Serial.println(readString);
+	  
+	    if (readString =="FORWARD"){
+        Serial.println("I received FORWARD!! ");       
+		    L_Motor -> run(FORWARD);
+			  R_Motor -> run(FORWARD);
+			  L_Motor -> setSpeed(F_Speed);
+			  R_Motor -> setSpeed(F_Speed);  
+	    }
+	    
+	    if (readString =="BACKWARD"){
+		    L_Motor -> run(BACKWARD);
+			  R_Motor -> run(BACKWARD);
+			  L_Motor -> setSpeed(B_Speed);
+			  R_Motor -> setSpeed(B_Speed);  
+	    }
+	
+	    if (readString =="LEFT"){
+		    L_Motor -> run(FORWARD);
+			  R_Motor -> run(FORWARD);
+			  L_Motor -> setSpeed(F_STOP);
+			  R_Motor -> setSpeed(F_Speed);  
+	    }
+	
+	    if (readString =="RIGHT"){
+		    L_Motor -> run(FORWARD);
+			  R_Motor -> run(FORWARD);
+			  L_Motor -> setSpeed(F_Speed);
+			  R_Motor -> setSpeed(F_STOP);  
+	    }
+	
+	    if (readString =="STOP"){
+	      L_Motor -> run (RELEASE);
+	      R_Motor -> run (RELEASE);
+	    }
+	
+	    if (readString == "GRAB"){
+	      grab_servo.write(90);
+	    }
+	
+	    if (readString == "DROP"){
+	      grab_servo.write(175);
+	    }
+	
+	    readString="";
+	} 
 }
