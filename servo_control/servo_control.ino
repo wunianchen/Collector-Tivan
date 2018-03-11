c2b9b393643136de8a6f062b31ec1d896fe8c57d
wunianchen <36548290+wunianchen@users.noreply.github.com>
2018-03-11 19:41:48 -0400

    Adding initial codes for distance sensor

--- servo_control/servo_control.ino
+++ servo_control/servo_control.ino
@@ -1,8 +1,11 @@
+// Header file for motor control
+#include <Servo.h>                                
+#include <Adafruit_MotorShield.h>                   
+#include "utility/Adafruit_MS_PWMServoDriver.h"
+
+// Header file for BLE
 #include <Wire.h>
 #include <SPI.h>
-#include <Servo.h>                                  // include the servo library
-#include <Adafruit_MotorShield.h>                   // include the motor shield library
-#include "utility/Adafruit_MS_PWMServoDriver.h"
 #include "Adafruit_BLE.h"
 #include "Adafruit_BluefruitLE_SPI.h"
 #include "Adafruit_BLuefruitLE_UART.h"
@@ -12,43 +15,51 @@
   #include <SoftwareSerial.h>
 #endif
 
-#define FACTORYRESET_ENABLE 1                       // Enable this will put BLE in a known good state
+// Constant for BLE
+#define FACTORYRESET_ENABLE 	1                       // enable this will put BLE in a known good state
+// Constant for servo
+#define SERVO_ATTACH_PIN		10						// digital#10 pin controls servo#1 input
+#define	INIT_SPEED				200                     // set the initial motor speed
+#define	F_SPEED					100                     // set the forward speed
+#define	B_SPEED					200				       	// set the backward speed
+#define	F_STOP					0					    // set the stop speed
+#define	SERVO_RELE_POS			175						// set the servo release position
+#define	SERVO_GRAB_POS			90						// set the servo grab position
+// Constant for LIDAR distance sensor
+#define VL6180X_I2C_ADDR		0x29					// the I2C address of distance sensor
 
 // Create the motor shield object with the default I2C address
 Adafruit_MotorShield AFMS = Adafruit_MotorShield();
 
 //Connect the two DC motor to port M1 & M2
-Adafruit_DCMotor *L_Motor = AFMS.getMotor(1);       // robot left motor
-Adafruit_DCMotor *R_Motor = AFMS.getMotor(2);       // robot right motor
-Servo grab_servo;                                   // define servo name
+Adafruit_DCMotor *L_Motor = AFMS.getMotor(1);       	// robot left motor
+Adafruit_DCMotor *R_Motor = AFMS.getMotor(2);       	// robot right motor
+Servo grab_servo;                                   	// define servo name
 
-int Init_Speed = 200;                               // set the initial motor speed
-int F_Speed = 100;                                  // set the forward speed
-int B_Speed = 200;				                          // set the backward speed
-int F_STOP = 0;					                            // set the stop speed
-String readString;                                  // Read data from UART/Bluetooth
+String readString;                                  	// Read data from UART/Bluetooth
 
 // Define bluetooth (SPI) using SCK/MOSI/MISO hardware SPI pins
 Adafruit_BluefruitLE_SPI ble(BLUEFRUIT_SPI_CS, BLUEFRUIT_SPI_IRQ, BLUEFRUIT_SPI_RST);
 
 void setup() {
   Serial.begin(115200);
-  Serial.println("Collector Tivan Initialization!!!");    // initialization message
+  Serial.println("Collector Tivan Initialization!!!");  // initialization message
 
   // Motor & Servo initialization
-  AFMS.begin();                                           // create with the default frequency 1.6 KHz.
-  grab_servo.attach(10);                                  // Digital 10 pin controls the Servo#1 input
-  grab_servo.write(175);                                  // Servo motor starting position
+  AFMS.begin();                                         // create with the default frequency 1.6 KHz.
+  grab_servo.attach(SERVO_ATTACH_PIN);                              
+  grab_servo.write(SERVO_RELE_POS);                    
 
   // Turn on left motor
-  L_Motor -> setSpeed(Init_Speed);
+  L_Motor -> setSpeed(INIT_SPEED);
   L_Motor -> run(RELEASE);
   
   // Turn on right motor
-  R_Motor -> setSpeed(Init_Speed);
+  R_Motor -> setSpeed(INIT_SPEED);
   R_Motor -> run(RELEASE);
   // Motor & Servo initialization ends
 
+  // Bluetooth initialization
   Serial.println("Adarduit Bluefruit Initializeation!!!");
   if(!ble.begin(VERBOSE_MODE))
   {
@@ -93,28 +104,28 @@ void loop() {
     if (readString == "FORWARD"){
 	    L_Motor -> run(FORWARD);
 		R_Motor -> run(FORWARD);
-		L_Motor -> setSpeed(F_Speed);
-		R_Motor -> setSpeed(F_Speed);  
+		L_Motor -> setSpeed(F_SPEED);
+		R_Motor -> setSpeed(F_SPEED);  
     }
     
     if (readString =="BACKWARD"){
 	    L_Motor -> run(BACKWARD);
 		R_Motor -> run(BACKWARD);
-		L_Motor -> setSpeed(B_Speed);
-		R_Motor -> setSpeed(B_Speed);  
+		L_Motor -> setSpeed(B_SPEED);
+		R_Motor -> setSpeed(B_SPEED);  
     }
 
     if (readString =="LEFT"){
 	    L_Motor -> run(FORWARD);
 		R_Motor -> run(FORWARD);
 		L_Motor -> setSpeed(F_STOP);
-		R_Motor -> setSpeed(F_Speed);  
+		R_Motor -> setSpeed(F_SPEED);  
     }
 
     if (readString =="RIGHT"){
 	    L_Motor -> run(FORWARD);
 		R_Motor -> run(FORWARD);
-		L_Motor -> setSpeed(F_Speed);
+		L_Motor -> setSpeed(F_SPEED);
 		R_Motor -> setSpeed(F_STOP);  
     }
 
@@ -124,11 +135,11 @@ void loop() {
     }
 
     if (readString == "GRAB"){
-      grab_servo.write(90);
+      grab_servo.write(SERVO_GRAB_POS);
     }
 
     if (readString == "DROP"){
-      grab_servo.write(175);
+      grab_servo.write(SERVO_RELE_POS);
     }
 
     readString="";
